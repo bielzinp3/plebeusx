0e73090becbcbabb48687d344d0c132ea062ca78
Alexandre Liberato <alexandreliberatto@gmail.com>
2024-07-22 22:11:49 -0300

    Criando página timeline

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -13,12 +13,46 @@ class AppController extends Action {
         session_start();
 
         if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+
+            //recuperação dos tweets
+            $tweet = Container::getModel('Tweet');
+
+            $tweet->__set('id_usuario', $_SESSION['id']);
+
+            $tweets = $tweet->getAll();
+
+            echo '<pre>';
+            print_r($tweets);
+            echo '</pre>';
+
+            $this->view->tweets = $tweets;
+
             $this->render('timeline');
         } else {
             header('Location: /?login=erro');
         }
 
         
+    }
+
+    public function tweet() {
+
+        session_start();
+
+        if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+
+           $tweet = Container::getModel('Tweet'); 
+           
+           $tweet->__set('tweet', $_POST['tweet']);
+           $tweet->__set('id_usuario', $_SESSION['id']);
+
+           $tweet->salvar();
+
+           header('Location: /timeline');
+
+        } else {
+            header('Location: /?login=erro');
+        }
     }
 
 }
